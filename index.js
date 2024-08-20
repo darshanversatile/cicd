741376cbd671b5eb62958c8b71d491e6b996b4eb
darshanversatile <darshan.versatileit@gmail.com>
2024-08-20 16:52:02 +0530

    c

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  const filePath = path.join(__dirname, "request-body.txt");
+
+  const requestBody = JSON.stringify(req.body, null, 2);
+
+  fs.writeFile(filePath, requestBody, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.send("Request body written to file successfully");
+  });
+});
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "request-body.txt");
+
+  const requestBody = JSON.stringify(req.body, null, 2);
+
+  fs.writeFile(filePath, requestBody, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.send("Request body written to file successfully");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
